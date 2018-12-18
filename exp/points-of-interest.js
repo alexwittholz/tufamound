6c04755d685795e977c1639aca49434d7a6b31f0
dansundy <dansundy@gmail.com>
2018-12-18 16:43:19 -0500

    Add navigation panel.

---
+++ exp/points-of-interest.js
@@ -0,0 +1,40 @@
+window.pointsOfInterest = (function() {
+  return [
+    {
+      id: 'bunny-bar',
+      name: 'Bunny Bar',
+      position: {
+        lng: -127.3257,
+        lat: 62.02515,
+        height: 500
+      }
+    },
+    {
+      id: 'rabitkettle-tufamound',
+      name: 'Rabitkettle',
+      position: {
+        lng: -127.18046,
+        lat: 61.94179,
+        height: 500
+      }
+    },
+    {
+      id: 'boat-on-the-river',
+      name: 'Boat on the River',
+      position: {
+        lng: -125.91153,
+        lat: 61.67374,
+        height: 500
+      }
+    },
+    {
+      id: 'virginia-falls',
+      name: 'Virginia Falls',
+      position: {
+        lng: -125.7356,
+        lat: 61.6081,
+        height: 500
+      }
+    }
+  ]
+})()
